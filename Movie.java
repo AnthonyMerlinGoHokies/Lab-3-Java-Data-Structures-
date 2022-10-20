af253f791521cc0b283b948ad971fe2f25e58b6e
Anthony Merlin <92953379+AnthonyMerlinGoHokies@users.noreply.github.com>
2022-10-20 12:03:24 -0400

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,24 @@
+package collections;
+
+/**
+ * Movie class
+ * 
+ * @author Anthony Merlin 
+ * @version 2022.09.08
+ *
+ */
+public class Movie extends MovieADT{
+    /**
+     * Movie object constructor
+     * 
+     * @param title
+     *            String title of the movie
+     */
+    public Movie(String title) {
+        super(title);
+    }
+
+}
+    
+    
+
